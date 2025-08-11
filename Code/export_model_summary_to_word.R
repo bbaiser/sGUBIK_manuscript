e7dfefc9ed8623080c9d575796d7a89a55e11ea3
Baiser <bbaiser@ufl.edu>
2025-08-11 16:44:16 -0400

    export model summary tables to word

---
+++ Code/export_model_summary_to_word.R
@@ -0,0 +1,39 @@
+library(phylolm)
+library(officer)
+library(flextable)
+library(dplyr)
+
+
+
+# Extract and clean summary
+summary_phylo <- summary(model_17)
+
+
+coef_df <- as.data.frame(summary_phylo$coefficients) %>%
+  rownames_to_column("Term") %>%
+  rename(
+    Estimate = Estimate,
+    `Std. Error` = StdErr,
+    `t value` = t.value,
+    `p value` = p.value
+  ) %>%
+  mutate(
+    Estimate = round(Estimate, 2),
+    `Std. Error` = round(`Std. Error`, 2),
+    `t value` = round(`t value`, 2),
+    `p value` = formatC(`p value`, format = "e", digits = 2)  # scientific notation
+  )
+
+# Create flextable
+ft <- flextable(coef_df) %>%
+  autofit() %>%
+  theme_vanilla() %>%
+  set_caption("Phylogenetic Linear Model Coefficients") %>%
+  align(align = "center", part = "all")
+
+# Export to Word
+doc <- read_docx() %>%
+  body_add_par("Phylogenetic Linear Model Summary", style = "heading 1") %>%
+  body_add_flextable(ft)
+
+print(doc, target = "phylolm_summary.docx")
